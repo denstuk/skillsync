2e07d8ca8454d1ddbc7b3f610cab154eb2022ebe
Denis Stuk <denstuk@outlook.com>
2024-06-11 13:42:41 +0300

    fix: merge issues

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -5,7 +5,7 @@ import { AppService } from './app.service';
 import { GeminiModule } from './gemini/gemini.module';
 import { ComposerModule } from './modules/composer/composer.module';
 import { TaskModule } from './modules/task/task.module';
-import { QuizModule } from './quiz/quizzes/quiz.module';
+import { QuizModule } from './quiz/quiz.module';
 
 @Module({
   imports: [
@@ -17,6 +17,7 @@ import { QuizModule } from './quiz/quizzes/quiz.module';
     QuizModule,
     TaskModule,
     ComposerModule,
+    QuizModule,
   ],
   controllers: [AppController],
   providers: [AppService],
